f5683ee5ab543bcbcae3172064361d8bcc193e51
Ridho Andriano <ridho.andriano17@gmail.com>
2023-01-29 17:40:10 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello,World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae ullam sint ipsa numquam esse nihil suscipit quam, veritatis nobis fuga eaque animi expedita consectetur necessitatibus at delectus ea mollitia. Ex.</p>
+    <ul>
+        <li>DIrawlhwlah</li>
+        <li>hsajdsjakdshad</li>
+    </ul>
 </body>
 </html>
